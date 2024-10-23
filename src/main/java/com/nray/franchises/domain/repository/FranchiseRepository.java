d40d0e0f643a178fe61d3f9ec01f6f9edc3d9080
Nestor Raul Agredo LLanten <nestoragredo93@gmail.com>
2024-10-23 08:44:20 -0500

    feat: add branch service

--- src/main/java/com/nray/franchises/domain/repository/FranchiseRepository.java
+++ src/main/java/com/nray/franchises/domain/repository/FranchiseRepository.java
@@ -1,8 +1,11 @@
 package com.nray.franchises.domain.repository;
 
+import com.nray.franchises.domain.model.Branch;
 import com.nray.franchises.domain.model.Franchise;
 import reactor.core.publisher.Mono;
 
 public interface FranchiseRepository {
     Mono<Franchise> addFranchise(Franchise franchise);
+
+    Mono<Branch> addBranch(Branch branch);
 }
